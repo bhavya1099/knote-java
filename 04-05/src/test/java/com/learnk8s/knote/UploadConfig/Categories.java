fb3bb072121722388f5fee21dc56c5e503e7fc00
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-06-27 10:12:39 +0530

    Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ 04-05/src/test/java/com/learnk8s/knote/UploadConfig/Categories.java
@@ -0,0 +1,21 @@
+package com.learnk8s.knote.UploadConfig;
+
+public interface Categories {
+
+	interface addResourceHandlers {
+
+	}
+
+	interface roostTest1 {
+
+	}
+
+	interface roostTest2 {
+
+	}
+
+	interface getUploadDir {
+
+	}
+
+}
