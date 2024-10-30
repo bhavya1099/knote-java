14da17c322a741355cbb1d5cdbe3ad08464e4fa2
bhavya1099 <bhavya.goswami99@gmail.com>
2024-10-30 05:27:43 +0000

    Unit test generated by RoostGPT
    Using AI Model gpt-4

---
+++ 04-05/src/test/java/com/learnk8s/knote/UploadConfig/Categories.java
@@ -0,0 +1,29 @@
+package com.learnk8s.knote.UploadConfig;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface addResourceHandlers {
+
+	}
+
+	interface getUploadDir {
+
+	}
+
+}
