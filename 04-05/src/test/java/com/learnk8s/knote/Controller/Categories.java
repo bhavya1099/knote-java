773f0b0e043c41767f1205493a6a006215e88c6c
bhavya1099 <bhavya.goswami99@gmail.com>
2024-12-06 15:52:26 +0530

    Unit test generated by RoostGPT
    Using AI Model gpt-3.5-turbo

---
+++ 04-05/src/test/java/com/learnk8s/knote/Controller/Categories.java
@@ -0,0 +1,41 @@
+package com.learnk8s.knote.Controller;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface index {
+
+	}
+
+	interface getAllNotes {
+
+	}
+
+	interface saveNotes {
+
+	}
+
+	interface uploadImage {
+
+	}
+
+	interface saveNote {
+
+	}
+
+}
